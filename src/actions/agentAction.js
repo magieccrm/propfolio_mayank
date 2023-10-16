2b9a414b5f4ecfaedc01280d9a07d67dc02a83b5
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-10-16 15:36:59 +0530

    aa

--- src/actions/agentAction.js
+++ src/actions/agentAction.js
@@ -8,25 +8,20 @@ import {
   import axios from "axios";
 
   export const login=(email,password)=>async (dispatch)=>{
-      
     try{
         dispatch({type:LOGIN_REQUEST});
         const config={Headers:{"Content-Type":"application/json"}};
-        
        const {data} =await axios.post(
         `https://crm-backend-1qcz.onrender.com/api/v1/agent_login`,
         {email,password},
         config
         );    
-        
-        dispatch({type:LOGIN_SUCCESS,payload:data.agent});
-
-
+             dispatch({type:LOGIN_SUCCESS,payload:data.agent});
     }catch(error){
        dispatch({type:LOGIN_FAIL,payload:error.response.data.message});
     }
   }
-
+  
    // clearing error  
  export const clearErrors= ()=>async (dispatch)=>{ 
     dispatch({type:CLEAR_ERRORS});
