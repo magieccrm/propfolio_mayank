26ef73ac41da7540fbce82b1514ddaa40131ba9f
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-12-17 17:43:33 +0530

    sssssss

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -8,25 +8,20 @@ function Notification(){
 const [notification,setNotification]=useState({title:"",body:""});
 
 useEffect(()=>{
-   requestPermission();
-
+        requestPermission();
+        // console.log("mmmm",currentToken)  
    const unsubscribe=onMessageListener().then(payload=>{
-
     setNotification({
         title:payload?.notification?.title,
         body:payload?.notification?.body
     });
-    
     toast.success(`${payload?.notification?.title}:${payload?.notification?.body}`,
    {
     duration:60000,
     position:"top-right"
    }
     )
-
-    
    })
-
     return ()=>{
         unsubscribe.catch(err=>console.log("Failed :",err));
     };
@@ -36,6 +31,5 @@ return (
         <Toaster/>
     </div>
 );
-
 }
 export default Notification;
